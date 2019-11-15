ead22993d5e3ead68a08359c154038ebe8114760
redrect <chenmrkent@hotmail.com>
2019-11-15 16:53:06 -0500

    create route and read json

---
+++ photographer/app/Http/Controllers/photographerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class photographerController extends Controller
+{
+    public function index(){
+
+        return view('photographer');
+    }
+
+    public function readJson(){
+
+        $json = Storage::disk('local')->get('landscapes.json');
+        $json = json_decode($json,true);
+
+        return $json;
+    }
+}
